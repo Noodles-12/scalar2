4a0751f9d4f95d38d37d7eabc67818766697c518
Enderic <133310359+Enderic@users.noreply.github.com>
2026-04-16 09:48:49 -0500

    add if_id

---
+++ scalar2.srcs/sim_1/new/fetch_decode_pl_tb.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module fetch_decode_pl_tb();
+    logic clk;
+    logic [0:30] instr_a;
+    logic [0:30] instr_b;
+    logic [0:5] opcode_a;
+    logic [0:5] opcode_b;
+
+    fetch_decode_pl fd(.clk(clk),
+                       .instr_a(instr_a),
+                       .instr_b(instr_b),
+                       .opcode_a(opcode_a),
+                       .opcode_b(opcode_b) );
+                       
+    initial clk = 0;
+    always #5 clk = ~clk;
+    
+    initial begin
+        instr_a = 30'b0000010000000000000000000000000; instr_b = 30'b0000010000000000000000000000000; # 10;
+        $finish;
+    end
+                  
+endmodule
