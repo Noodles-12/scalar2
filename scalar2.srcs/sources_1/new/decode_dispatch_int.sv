7c2bb9ef0fbd226a85f1f14177bdc22b577c2049
Noodles-12 <mahiyan21@gmail.com>
2026-04-19 17:55:27 -0500

    add pc and instruction mem; successful tb; reduce pl to 1 cycle in between

--- scalar2.srcs/sources_1/new/decode_dispatch_int.sv
+++ scalar2.srcs/sources_1/new/decode_dispatch_int.sv
@@ -1,24 +1,20 @@
 `timescale 1ns / 1ps
 
+import config_pkg::*;
+
 module decode_dispatch_int(instr_a, instr_b, clk,
                             opcode_a, instr_dest_a, instr_src1_a, instr_src2_a,
                             opcode_b, instr_dest_b, instr_src1_b, instr_src2_b);
-    input [0:29] instr_a;
-    input [0:29] instr_b;
-    input clk;
-    
-    output logic [0:5] opcode_a;
-    output logic [0:3] instr_dest_a;
-    output logic [0:3] instr_src1_a;
-    output logic [0:3] instr_src2_a;
-    
-    output logic [0:5] opcode_b;
-    output logic [0:3] instr_dest_b;
-    output logic [0:3] instr_src1_b;
-    output logic [0:3] instr_src2_b;
-    
-    logic [0:29] instr_a_reg;
-    logic [0:29] instr_b_reg;
+                            
+    input logic [0:INSTR_WIDTH - 1] instr_a, instr_b;
+    input logic clk;
+
+    output logic [0:5] opcode_a, opcode_b;
+    output logic [0:3] instr_dest_a, instr_dest_b;
+    output logic [0:3] instr_src1_a, instr_src1_b;
+    output logic [0:3] instr_src2_a, instr_src2_b;
+
+    logic [0:INSTR_WIDTH - 1] instr_a_reg, instr_b_reg;
     
     always_ff @ (posedge clk) begin
         instr_a_reg <= instr_a;
