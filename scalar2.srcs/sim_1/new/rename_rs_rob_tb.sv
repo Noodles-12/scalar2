ebca7106b0f51d3f92dc13355a0b1da84f4e5bd4
Noodles-12 <mahiyan21@gmail.com>
2026-04-29 17:04:13 -0500

    testbench stuff

--- scalar2.srcs/sim_1/new/rename_rs_rob_tb.sv
+++ scalar2.srcs/sim_1/new/rename_rs_rob_tb.sv
@@ -5,17 +5,17 @@ module rename_rs_rob_tb();
     logic clk;
     instruction instr_a, instr_b;
 
-    rob_entry rob_a, rob_b;
     rs_entry rename_a, rename_b;
+    rob_entry rob_a, rob_b; 
 
-    rs_entry rs_entry_a [0:3], rs_entry_b [0:3];
-    rob_entry rob_entry_a, rob_entry_b;
+    //rs_entry rs_entry_a [0:3], rs_entry_b [0:3];
+    //rob_entry rob_entry_a, rob_entry_b;
 
-    rob_entry output_rob [0:3];
+    // rob_entry output_rob [0:3];
 
-    int_rs_entry intrs_op_a, intrs_op_b;
+    // int_rs_entry intrs_op_a, intrs_op_b;
 
-    logic almost_full_int, almost_full_imm;
+    // logic almost_full_int, almost_full_imm;
 
     reg_file rf(.clk(clk),
                 .og_instr_a(instr_a),
@@ -25,6 +25,7 @@ module rename_rs_rob_tb();
                 .rob_a(rob_a),
                 .rob_b(rob_b) );
 
+    /*
     rename_dispatch_pl dispatch(.clk(clk),
                                 .rename_a(rename_a),
                                 .rename_b(rename_b),
@@ -34,7 +35,7 @@ module rename_rs_rob_tb();
                                 .rs_op_b(rs_entry_b),
                                 .rob_op_a(rob_entry_a),
                                 .rob_op_b(rob_entry_b) );
-
+                                
     reorder_buffer rob(.clk(clk),
                        .input_a(rob_entry_a),
                        .input_b(rob_entry_b),
@@ -46,12 +47,15 @@ module rename_rs_rob_tb();
                            .almost_full(almost_full_int),
                            .output_a(intrs_op_a),
                            .output_b(intrs_op_b) );
+    */
 
     initial clk = 0;
     always #5 clk = ~clk;
 
     initial begin
-        
+        instr_a = 30'b000001_0001_0010_0011_000000000000;                 // add r1, r2, r3
+        instr_b = 30'b000011_0100_0001_0110_000000000000; #10; #10; #10;  // sub r4, r1, r3 RAW
+        $finish;
     end
 
 endmodule
