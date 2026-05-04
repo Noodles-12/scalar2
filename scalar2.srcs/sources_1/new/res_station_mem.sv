e6cbb6992eb52b96dbe9276e899a2cd70ae0c49e
Enderic <133310359+Noodles-12@users.noreply.github.com>
2026-05-04 09:41:39 -0500

    change store buffers to dispatch out of order

--- scalar2.srcs/sources_1/new/res_station_mem.sv
+++ scalar2.srcs/sources_1/new/res_station_mem.sv
@@ -12,22 +12,18 @@ module res_station_mem(clk, instr_a, instr_b, cdb_arr,
 
     rs_entry instr_a_reg, instr_b_reg;
 
-    // Should be treated like a queue/FIFO array
-    // Stores forced to execute in-order
+    // Both load & stores can happen out of order
     store_rs_entry store_buffer [0:7];  
     store_rs_entry next_store_buffer [0:7];
 
-    // Can act like a regular RS
-    // Loads can happen out-of-order as long as no previous stores (tracked by count)
     load_rs_entry load_buffer [0:7];
     load_rs_entry next_load_buffer [0:7];
     
-    logic [0:3] store_head = 0, store_tail = 0, store_count = 0;
-    logic [0:3] next_store_head, next_store_tail, next_store_count;
-
+    logic [0:3] store_count = 0, next_store_count = 0;
     logic [0:3] load_count = 0, next_load_count = 0;
 
-    logic done, head_done;
+    logic done;
+    logic done_a, done_b;
 
     initial begin
         for(int i = 0; i < 8; i++)
@@ -41,10 +37,7 @@ module res_station_mem(clk, instr_a, instr_b, cdb_arr,
         store_buffer <= next_store_buffer;
         load_buffer <= next_load_buffer;
 
-        store_head <= next_store_head;
-        store_tail <= next_store_tail;
         store_count <= next_store_count;
-
         load_count <= next_load_count;
     end
 
@@ -52,42 +45,29 @@ module res_station_mem(clk, instr_a, instr_b, cdb_arr,
         next_store_buffer = store_buffer;
         next_load_buffer = load_buffer;
 
-        next_store_head = store_head;
-        next_store_tail = store_tail;
         next_store_count = store_count;
-
         next_load_count = load_count;
 
         str_op_a = '0;
         str_op_b = '0;
-        head_done = 0;
+        
+        done_a = 0;
+        done_b = 0;
 
-        // Pushing first ready store
-        if(next_store_buffer[next_store_head].check1 && next_store_buffer[next_store_head].check2) begin
-            str_op_a = next_store_buffer[next_store_head];
-            next_store_buffer[next_store_head] = 0;
-            next_store_head = (next_store_head == 7) ? 0 : next_store_head + 1;
-            if(next_store_count > 0) next_store_count--;
-            head_done = 1;
-
-            // Decrement the count on each load
-            for(int i = 0; i < 8; i++) begin
-                if(next_load_buffer[i] == 0 || next_load_buffer[i].count == 0) continue;
-                next_load_buffer[i].count--;
-            end
-        end
-
-        // Pushes next one if possible
-        if(head_done && next_store_buffer[next_store_head].check1 && next_store_buffer[next_store_head].check2) begin
-            str_op_b = next_store_buffer[next_store_head];
-            next_store_buffer[next_store_head] = 0;
-            next_store_head = (next_store_head == 7) ? 0 : next_store_head + 1;
-            if(next_store_count > 0) next_store_count--;
-
-            // Decrement the count on each load
-            for(int i = 0; i < 8; i++) begin
-                if(next_load_buffer[i] == 0 || next_load_buffer[i].count == 0) continue;
-                next_load_buffer[i].count--;
+        // Dispatching ready stores
+        for(int i = 0; i < 8; i++) begin
+            if(next_store_buffer[i].check1 && next_store_buffer[i].check2) begin
+                if(!done_a) begin
+                    str_op_a = next_store_buffer[i];
+                    done_a = 1;
+                    next_store_buffer[i] = 0;
+                end 
+                
+                if (done_a && !done_b) begin
+                    str_op_b = next_store_buffer[i];
+                    done_b = 1;
+                    next_store_buffer[i] = 0;
+                end
             end
         end
 
@@ -105,9 +85,14 @@ module res_station_mem(clk, instr_a, instr_b, cdb_arr,
                 end
             end
             29 : begin
-                next_store_buffer[next_store_tail] = instr_a_reg;
-                next_store_tail = (next_store_tail == 7) ? 0 : next_store_tail + 1;
-                next_store_count++;
+                done = 0;
+                for(int i = 0; i < 8; i++) begin
+                    if(!done && (store_buffer[i] == 0)) begin
+                        next_store_buffer[i] = instr_a_reg;
+                        next_store_count++;
+                        done = 1;
+                    end
+                end
             end
         endcase
 
@@ -125,9 +110,14 @@ module res_station_mem(clk, instr_a, instr_b, cdb_arr,
                 end
             end
             29 : begin
-                next_store_buffer[next_store_tail] = instr_b_reg;
-                next_store_tail = (next_store_tail == 7) ? 0 : next_store_tail + 1;
-                next_store_count++;
+                done = 0;
+                for(int i = 0; i < 8; i++) begin
+                    if(!done && (store_buffer[i] == 0)) begin
+                        next_store_buffer[i] = instr_b_reg;
+                        next_store_count++;
+                        done = 1;
+                    end
+                end
             end
         endcase
     end
