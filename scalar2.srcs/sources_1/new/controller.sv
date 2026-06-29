bb2d7796244fc0eeacdef4af7544f22c3265707b
Enderic <mahiyan21@gmail.com>
2026-06-29 10:35:57 -0500

    branch predictor pprogress

---
+++ scalar2.srcs/sources_1/new/controller.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/29/2026 10:12:17 AM
+// Design Name: 
+// Module Name: controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module controller(
+
+    );
+endmodule
