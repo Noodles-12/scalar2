b518616f75b91b0fdd63403dc3012eb984bebcab
Enderic <mahiyan21@gmail.com>
2026-06-29 10:37:41 -0500

    branch predictor pprogress

--- scalar2.srcs/sources_1/new/branch_predictor.sv
+++ scalar2.srcs/sources_1/new/branch_predictor.sv
@@ -9,10 +9,17 @@ module branch_predictor(
     input instruction instr_a,
     input instruction instr_b
 );
-    logic [1:0] history_table [0:31];
 
-    logic is_branch_a, is_branch_b; // Represents conditional branches
-    logic is_jump_a, is_jump_b;     // Represents unconditional branches
+    typedef enum logic [1:0] {
+        NORMAL          = 2'b00,
+        JUMP            = 2'b01,
+        UNTAKEN_BRANCH  = 2'b10,
+        TAKEN_BRANCH    = 2'b11
+    } instr_code;
+    
+    logic [1:0] history_table [0:31];
+    
+    instr_code code_a, code_b;
 
     always_ff @ (posedge clk) begin
         if(rst) begin
@@ -22,7 +29,39 @@ module branch_predictor(
         end
     end
 
+    // Instruction A code
     always_comb begin
-        is_branch_a = 0; is_branch_b = 0;
+        case(instr_a.opcode) inside
+            [6'b000001:6'b011011] : begin
+                code_a = NORMAL;
+            end
+            
+            [6'b011100:6'b100001] : begin
+            
+            end
+            
+            [6'b100010:6'b100011] : begin
+                code_a = JUMP;
+            end
+            default: code_a = NORMAL;
+        endcase
+    end
+    
+    // Instruction B code
+    always_comb begin
+        case(instr_b.opcode) inside
+            [6'b000001:6'b011011] : begin
+                code_b = NORMAL;
+            end
+            
+            [6'b011100:6'b100001] : begin
+            
+            end
+            
+            [6'b100010:6'b100011] : begin
+                code_b = JUMP;
+            end
+            default: code_b = NORMAL;
+        endcase
     end
 endmodule
