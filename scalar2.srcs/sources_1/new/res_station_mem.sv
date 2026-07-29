e7b51eca47c16332e857819de5fb1fea0fcd470c
Noodles-12 <mahiyan21@gmail.com>
2026-07-29 17:16:58 -0500

    res_station_mem progress

--- scalar2.srcs/sources_1/new/res_station_mem.sv
+++ scalar2.srcs/sources_1/new/res_station_mem.sv
@@ -5,6 +5,7 @@ import config_pkg::*;
 module res_station_mem(
     input logic clk,
     input logic rst,
+    input logic mispredict_signal, 
     input rs_entry instr_a,
     input rs_entry instr_b,
     input cdb_entry cdb_arr [0:CDB_SIZE - 1],
@@ -23,20 +24,31 @@ module res_station_mem(
     output load_rs_entry load_disp_imm
 );
 
+    typedef struct packed {
+        logic valid;
+        logic is_store;
+        logic [4:0] idx;
+        store_rs_entry entry;
+    } insert_req;
+
     // Both load & stores can happen out of order
     store_rs_entry store_buffer [0:7];  
     logic [2:0] s_head, s_tail;
+    logic [2:0] s_tail_comb;
+
+    insert_req insert_a, insert_b;
 
     load_rs_entry load_buffer [0:7];
     logic [2:0] l_head, l_tail;
 
-    logic [3:0] store_count, load_count;
+    logic [3:0] store_count, store_insert, store_dispatch;
+    logic [3:0] load_count, load_insert, load_dispatch;
 
     logic [11:0] store_eff_addr [0:7];
-    logic store_valid_addr [0:7];
+    logic [0:7] store_valid_addr;
 
     logic [11:0] load_eff_addr [0:7];
-    logic load_valid_addr [0:7];
+    logic [0:7] load_valid_addr;
 
 
     always_ff @ (posedge clk) begin
@@ -67,7 +79,36 @@ module res_station_mem(
     end
 
     always_comb begin
+        load_insert = '0;
+        store_insert = '0;
         
+        s_tail_comb = s_tail;
+
+        case(instr_a.load_rs.opcode)
+            26 : begin
+                load_insert = load_insert + 1'b1;
+            end
+            27 : begin
+                store_insert = store_insert + 1'b1;
+                insert_a.valid = 1;
+                insert_a.idx = s_tail_comb;
+                insert_a.entry = instr_a;
+                s_tail_comb = s_tail_comb + 1'b1;
+            end
+        endcase
+
+        case(instr_b.load_rs.opcode)
+            26 : begin
+                load_insert = load_insert + 1'b1;
+            end
+            27 : begin
+                store_insert = store_insert + 1'b1;
+                insert_b.valid = 1;
+                insert_b.idx = s_tail_comb;
+                insert_b.entry = instr_b;
+                s_tail_comb = s_tail_comb + 1'b1;
+            end
+        endcase
     end
 
     /* // 1 Insert Entries
